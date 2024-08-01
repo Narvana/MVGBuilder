974c2dd13b749ca4e4441da096afc5d0e8e8456a
Narvana <adhikarijagdish3@gmail.com>
2024-08-01 17:29:43 +0530

    agent sales year and month added

--- app/Http/Controllers/AgentIncomeController.php
+++ app/Http/Controllers/AgentIncomeController.php
@@ -3,10 +3,76 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Plot_Sale;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
+
 
 class AgentIncomeController extends Controller
 {
     //
     // public function createIncome()
+    public function agentIncome(Request $request)
+    {
+
+        $params=$request->query('agent_id');
+
+        $income = DB::table('agent_incomes')
+        ->leftJoin('plot_sales', 'agent_incomes.plot_sale_id', '=', 'plot_sales.id')
+        ->leftJoin('plots', 'plot_sales.plot_id', '=', 'plots.id')
+        ->select(
+                 'plot_sales.plot_id',
+                 'plots.plot_No',
+                 'plots.plot_type',
+                    'plot_sales.totalAmount',
+                    'agent_incomes.total_income',
+                    'agent_incomes.tds_deduction',
+                    'agent_incomes.final_income',
+                    'agent_incomes.transaction_status',
+                )->where('agent_incomes.final_agent',$params)->get();
+
+        if($income->isEmpty())
+        {
+            return response()->json(['success' => 0,'error' =>"Data don't Exist"],400);
+        }
+
+                return response()->json(['success'=>1,'Incomes'=>$income],200);
+    }
+
+    public function agentSales(Request $request)
+    {
+        $user=Auth::guard('sanctum')->user();
+
+        // $sales = DB::table('agent_registers')
+        // ->leftJoin('plot_sales', 'agent_registers.id', '=', 'plot_sales.agent_id')
+        // ->select('agent_registers.id', 'plot_sales.totalAmount')
+        // ->where('agent_registers.id', $user->id)
+        // ->where('plot_sales.created_at');
+
+        $sales = DB::table('plot_sales')
+        ->leftJoin('agent_registers', 'plot_sales.agent_id', '=', 'agent_registers.id')
+        ->select(
+            DB::raw('YEAR(plot_sales.created_at) as year'),
+            DB::raw('MONTH(plot_sales.created_at) as month'),
+            DB::raw('SUM(plot_sales.totalAmount) as totalAmount')
+        )
+        ->where('agent_registers.id', $user->id)
+        ->groupBy(DB::raw('YEAR(plot_sales.created_at)'))
+        ->groupBy(DB::raw('MONTH(plot_sales.created_at)'))
+        ->orderBy(DB::raw('YEAR(plot_sales.created_at)'))
+        ->orderBy(DB::raw('MONTH(plot_sales.created_at)'))
+        ->get();
+        
+        $total=$sales->sum('totalAmount');
+        // if($sales->isEmpty())
+        // {
+        //     return response()->json(['success' => 0,'error' =>"Data don't Exist"],400);
+        // }
+
+        return response()->json(['success'=>1,'total'=>$total,'Sales'=>$sales],200);        
+    }
+
 }
