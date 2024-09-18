a247cc7bf1d44664e855d8bca38461a246343813
Narvana <adhikarijagdish3@gmail.com>
2024-09-18 12:25:46 +0530

    Client EMI info Model added

---
+++ app/Models/ClientEMIInfo.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ClientEMIInfo extends Model
+{
+    use HasFactory;
+
+    protected $fillable=[
+        'plot_sale_id',
+        'EMI_Amount',
+        'EMI_Date',
+    ];
+}
